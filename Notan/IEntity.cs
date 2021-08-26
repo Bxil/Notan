073cb6e46be8014ab7406e831e0473ea64c1638c
Bxil <ryansmake@gmail.com>
2021-08-26 23:20:17 +0200

    Removed OnNetworkCreate.

--- Notan/IEntity.cs
+++ Notan/IEntity.cs
@@ -8,7 +8,6 @@ namespace Notan
 
         void Serialize<T>(T serializer) where T : ISerializer;
         void Deserialize<T>(T deserializer) where T : IDeserializer<T>;
-        void OnNetworkCreate() { }
         void OnDestroy() { }
     }
 }
